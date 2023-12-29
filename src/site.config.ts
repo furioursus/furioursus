4a4e9f8f06a145b8d5848531e13abf81bb849c1a
Christopher Kennedy-Nuñez <yo@furioursus.dev>
2023-12-29 12:47:48 -0500

    🐛 (seo) fix font import for og-image creation

--- src/site.config.ts
+++ src/site.config.ts
@@ -2,11 +2,11 @@ import type { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "Christopher Robert Kennedy-Nuñez",
+	author: "Christopher Kennedy-Nuñez",
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
 	title: "Furioursus",
 	// Meta property used as the default description meta property
-	description: "Personal Website of Christopher Robert Kennedy-Nuñez",
+	description: "Personal Website of Christopher Kennedy-Nuñez",
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: "en-US",
 	// Meta property, found in src/components/BaseHead.astro L:42
